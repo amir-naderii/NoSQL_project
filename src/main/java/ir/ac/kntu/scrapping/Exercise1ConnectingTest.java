159933e6dd515c5c33fad2c238660d1f1c928b08
amirn <amirnaderi3059@yahoo.com>
2022-01-28 19:48:46 +0330

    connection established

---
+++ src/main/java/ir/ac/kntu/scrapping/Exercise1ConnectingTest.java
@@ -0,0 +1,20 @@
+package ir.ac.kntu.scrapping;
+
+import com.mongodb.MongoClient;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class Exercise1ConnectingTest {
+    @Test
+    public void shouldCreateANewMongoClientConnectedToLocalhost() throws Exception {
+        // When
+        // TODO: get/create the MongoClient
+        MongoClient mongoClient = null;
+
+        // Then
+        assertThat(MongoDBSetUp.getInstance(), is(notNullValue()));
+    }
+}
