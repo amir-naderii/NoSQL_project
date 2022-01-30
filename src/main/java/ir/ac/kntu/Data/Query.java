4d1a8eb121ff472e061d79e9bb7cf916e0bc75e1
user <negiiinn.nm@gmail.com>
2022-01-30 13:38:43 +0330

    convert to aggregation

--- src/main/java/ir/ac/kntu/Data/Query.java
+++ src/main/java/ir/ac/kntu/Data/Query.java
@@ -1,17 +1,11 @@
 package ir.ac.kntu.Data;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.*;
-import org.bson.BSON;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
-import java.io.File;
 import java.util.*;
 
 public class Query {
@@ -21,117 +15,131 @@ public class Query {
         in = new Scanner(System.in);
     }
 
-    public Document firstQuery(Date date) {
+    public List<Document> firstQuery(Date date) {
         Calendar c = Calendar.getInstance();
         c.setTime(date);
         c.add(Calendar.DATE, 1);
         Date nextDate = c.getTime();
-        return  new Document("$and", Arrays.asList(
+        return Arrays.asList(new Document("$match", new Document("$and", Arrays.asList(
                 new Document("flightDate", new Document("$gte", date)),
-                new Document("flightDate", new Document("$lt", nextDate))));
+                new Document("flightDate", new Document("$lt", nextDate))))));
 
     }
 
 
 
-    public Document secondQuery(int priceFrom, int priceTo) {
-        return new Document("$and", Arrays.asList(
+    public List<Document> secondQuery(int priceFrom, int priceTo) {
+        return Arrays.asList(new Document("$match", new Document("$and", Arrays.asList(
                 new Document("cost", new Document("$gte", priceFrom)),
-                new Document("cost", new Document("$lt", priceTo))));
+                new Document("cost", new Document("$lt", priceTo))))));
     }
 
-//    public DBCursor thirdQuery(MongoCollection collection, String departure, String destination) {
-////        Bson match = Aggregates.match(
-////                Filters.eq("", countryId)
-////        BasicDBObject thirdQuery = new BasicDBObject( "$group",new BasicDBObject("$and", Arrays.asList(
-////                new BasicDBObject("departure.city", departure),
-////                new BasicDBObject("destination.city", destination))));
-//        return collection.find(thirdQuery);
-//    }
+    public List<Document> thirdQuery(String departure, String destination) {
+        return Arrays.asList(
+                new Document("$match", new Document("$and", Arrays.asList(new Document("destination.city",destination)
+                        ,new Document("departure.city",departure)))),
+                new Document("$group",new Document("_id", null)
+                        .append("min", new Document("$min", "$cost"))
+                        .append("max", new Document("$max", "$cost"))),
+                new Document("$project", new Document("_id", 0)
+                ));
+    }
 
-//    public DBCursor forthQuery(DBCollection collection,)
-//    }
+    public List<Document> forthQuery(String departure, String destination) {
+        return Arrays.asList(
+                new Document("$match", new Document("$and", Arrays.asList(
+                        new Document("destination.city",destination)
+                        ,new Document("departure.city",departure)))),
+                new Document("$group",new Document("_id", null)
+                        .append("sum", new Document("$sum", "$cost"))
+                        .append("avg", new Document("$avg", "$cost"))),
+                new Document("$project", new Document("_id", 0)
+                ));
+    }
 
+//    public Document fifthQuery(String flightType, Date date, int priceFrom,
+//                                int priceTo, String departure, String destination,String avrOrMax){
+//        Document fifthQuery = null;
+//        if(date != null) {
+//            fifthQuery = new Document("$and", Arrays.asList(
+//                    new Document("flightType", flightType),
+//                    firstQuery(date)));
+//        }else if(priceFrom != 0 && priceTo != 0){
+//            fifthQuery = new Document("$and", Arrays.asList(
+//                    new Document("flightType", flightType),
+//                    secondQuery( priceFrom, priceTo)));}
+//        else if(avrOrMax.equals("avr")){
+//            fifthQuery = new Document("$and", Arrays.asList(
+//                    new Document("flightType", flightType),
+//                    forthQuery(departure, destination)));
+//        }else if(avrOrMax.equals("max")){
+//            fifthQuery = new BasicDBObject("$and", Arrays.asList(
+//                    new Document("flightType", flightType),
+//                    thirdQuery(departure, destination)));
+//        }
 //
-    public Document fifthQuery(String flightType, Date date, int priceFrom,
-                                int priceTo, String departure, String destination,String avrOrMax){
-        Document fifthQuery = null;
-        if(date != null) {
-            fifthQuery = new Document("$and", Arrays.asList(
-                    new Document("flightType", flightType),
-                    firstQuery(date)));
-        }else if(priceFrom != 0 && priceTo != 0){
-            fifthQuery = new Document("$and", Arrays.asList(
-                    new Document("flightType", flightType),
-                    secondQuery( priceFrom, priceTo)));}
-        else if(avrOrMax.equals("avr")){
-            fifthQuery = new Document("$and", Arrays.asList(
-                    new Document("flightType", flightType),
-                    forthQuery(departure, destination)));
-        }else if(avrOrMax.equals("max")){
-            fifthQuery = new BasicDBObject("$and", Arrays.asList(
-                    new Document("flightType", flightType),
-                    thirdQuery(departure, destination)));
-        }
+//        return fifthQuery;
+//    }
 
-        return fifthQuery;
-    }
-
-    public FindIterable sixthQuery(int priceFrom,
+    public List<Document> sixthQuery(int priceFrom,
                                int priceTo, String departure, String destination){
-        Document sixthQuery = new Document("$and", Arrays.asList(
-                new Document("departure.city",departure),
-                new Document("destination.city",destination),
-                new Document("cost", new Document("$gte", priceFrom)),
-                new Document("cost", new Document("$lt", priceTo))));
+        return Arrays.asList(new Document("$match", new Document("$and", Arrays.asList(
+                new Document("destination.city",destination)
+                , new Document("departure.city",departure)
+                , new Document("cost", new Document("$gte", priceFrom))
+                , new Document("cost", new Document("$lt", priceTo)))))
+                , new Document("$group", new Document("_id", null)
+                        .append("min", new Document("$min", "$cost")))
+                ,new Document("$project", new Document("_id", 0))
+        );
     }
 
-    public Document seventhQuery(String departure, String destination, int capacity){
-        return new Document("$and",Arrays.asList(
+    public List<Document> seventhQuery(String departure, String destination, int capacity){
+        return Arrays.asList(new Document("$match", new Document("$and",Arrays.asList(
                 new Document("departure.city", departure),
                 new Document("destination.city", destination),
-                new Document("capacity",capacity)));
+                new Document("capacity",capacity)))));
     }
 
-    public Document eighthQuery(Date dateFrom, Date dateTo, String departure, String destination, int capacity,
-                                int priceFrom, int priceTo){
-        Document eighthQuery = null;
-        if(capacity == 0){
-            eighthQuery = new Document("$and",Arrays.asList(
-                    new Document("flightDate", new Document("$gte", dateFrom)),
-                    new Document("flightDate", new Document("$lt", dateTo)),
-                    sixthQuery(priceFrom,priceTo,departure,destination)
-            ));
-        }else if(priceTo == 0){
-            eighthQuery = new Document("$and",Arrays.asList(
-                    new Document("flightDate", new Document("$gte", dateFrom)),
-                    new Document("flightDate", new Document("$lt", dateTo)),
-                    seventhQuery(departure,destination,capacity)));
-        }
-        return eighthQuery;
-    }
+//    public Document eighthQuery(Date dateFrom, Date dateTo, String departure, String destination, int capacity,
+//                                int priceFrom, int priceTo){
+//        Document eighthQuery = null;
+//        if(capacity == 0){
+//            eighthQuery = new Document("$and",Arrays.asList(
+//                    new Document("flightDate", new Document("$gte", dateFrom)),
+//                    new Document("flightDate", new Document("$lt", dateTo)),
+//                    sixthQuery(priceFrom,priceTo,departure,destination)
+//            ));
+//        }else if(priceTo == 0){
+//            eighthQuery = new Document("$and",Arrays.asList(
+//                    new Document("flightDate", new Document("$gte", dateFrom)),
+//                    new Document("flightDate", new Document("$lt", dateTo)),
+//                    seventhQuery(departure,destination,capacity)));
+//        }
+//        return eighthQuery;
+//    }
 
-    public Document ninthQuery(String departure, String destination, Date date){
+    public List<Document> ninthQuery(String departure, String destination, Date date){
         Calendar c = Calendar.getInstance();
         c.setTime(date);
         c.add(Calendar.DATE, 1);
         Date nextDate = c.getTime();
-        return new Document("$and",Arrays.asList(
+        return Arrays.asList(new Document("$match", new Document("$and",Arrays.asList(
                 new Document("departure.city", departure),
                 new Document("destination.city", destination),
                 new Document("flightDate", new Document("$gte", date)),
-                new Document("flightDate", new Document("$lt", nextDate))));
+                new Document("flightDate", new Document("$lt", nextDate))))));
     }// add this to the end of find in helper function .projection(Projections.include("airline"))
 
-    public Document tenthQuery(Date date, String airline){
+    public List<Document> tenthQuery(Date date, String airline){
         Calendar c = Calendar.getInstance();
         c.setTime(date);
         c.add(Calendar.DATE, 1);
         Date nextDate = c.getTime();
-        return new Document("$and",Arrays.asList(
+        return Arrays.asList(new Document("$match", new Document("$and",Arrays.asList(
                 new Document("airline",airline),
                 new Document("flightDate", new Document("$gte", date)),
-                new Document("flightDate", new Document("$lt", nextDate))));
+                new Document("flightDate", new Document("$lt", nextDate))))));
     }// this query is for deletion
 
     public Bson updateCapacity(int newCapacity){
@@ -139,35 +147,36 @@ public class Query {
         return update;
     }
 
-    public Document eleventhQuery(String flightId){
-        return new Document("flightId",flightId);
+    public List<Document> eleventhQuery(String flightId){
+        return Arrays.asList(new Document("$match", new Document("flightId",flightId)));
     }
     // collection.updateOne(query.eleventhQuery(flightId,query.updateCapacity(newCapacity);
 
-    public Document twelfthQuery(String airline, Date date, String departure, String destination){
+    public List<Document> twelfthQuery(String airline, Date date, String departure, String destination){
         Calendar c = Calendar.getInstance();
         c.setTime(date);
         c.add(Calendar.DATE, 1);
         Date nextDate = c.getTime();
-        return new Document("$and",Arrays.asList(
+        return Arrays.asList(new Document("$match", new Document("$and",Arrays.asList(
                 new Document("departure.city", departure),
                 new Document("destination.city", destination),
                 new Document("flightDate", new Document("$gte", date)),
-                new Document("flightDate", new Document("$lt", nextDate))
-        ));
+                new Document("flightDate", new Document("$lt", nextDate)),
+                new Document("airline", airline)
+        ))));
     }
     //collection.updateOne(query.twelfthQuery(flightId,query.updateCapacity(newCapacity);
 
-    public Document thirteenthQuery(String departureCon, String destinationCon, Date date){
+    public List<Document> thirteenthQuery(String departureCon, String destinationCon, Date date){
         Calendar c = Calendar.getInstance();
         c.setTime(date);
         c.add(Calendar.DATE, 1);
         Date nextDate = c.getTime();
-        return new Document("$and",Arrays.asList(
+        return Arrays.asList(new Document("$match", new Document("$and",Arrays.asList(
                 new Document("departure.country", departureCon),
                 new Document("destination.country", destinationCon),
                 new Document("flightDate", new Document("$gte", date)),
-                new Document("flightDate", new Document("$lt", nextDate))));
+                new Document("flightDate", new Document("$lt", nextDate))))));
     }
     // .projection(Projections.include("departure.airport","destination.airport))
 
--- src/main/java/ir/ac/kntu/scrapping/Main.java
+++ src/main/java/ir/ac/kntu/scrapping/Main.java
@@ -1,17 +1,12 @@
 package ir.ac.kntu.scrapping;
 
 import com.mongodb.*;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Projections;
-import ir.ac.kntu.Data.Flight;
 import ir.ac.kntu.Data.Query;
-import ir.ac.kntu.Data.RandomDataGenerator;
 import org.bson.Document;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 
 public class Main {
@@ -19,20 +14,17 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 //        RandomDataGenerator randomDataGenerator = new RandomDataGenerator();
 //        randomDataGenerator.generate(100);
-        MongoCollection collection = MongoDBSetUp.getInstance().getDatabase("FlightDataBase").getCollection("flights");
+        MongoCollection<Document> collection = MongoDBSetUp.getInstance().getDatabase("FlightDataBase").getCollection("flights");
         Query query = new Query();
         Date date = new Date(new Date().getYear(),Calendar.FEBRUARY,7,0,0);
         Document query11 = new Document("id","563");
-        FindIterable Iter = query.sortQuery(collection,query.firstQuery(date),true,false);
-        int cnt = 0;
-        Iter.forEach((Block<? super Document>)
-                doc -> System.out.println(doc.toJson()));
-//        System.out.println(cursor.size());
-//        while(cursor.size() > cnt){
-//            System.out.println("boozh boozh");
-//            System.out.println(cursor.next().get("flightId").toString());
-//            cnt++;
-        }
-
+        collection.aggregate(query.sixthQuery(300, 500,"Isfahan", "Doha"))
+                .forEach((Block<? super Document>)
+                        doc -> System.out.println(doc.toJson()));
+//        int cnt = 0;
+//        Iter.forEach((Block<? super Document>)
+//                doc -> System.out.println(doc.toJson()));
     }
+
+}
 
